7957cf3e4a5e2ad5fd8842609ba4d7934f27e654
jyothikorrapati123 <144519938+jyothikorrapati123@users.noreply.github.com>
2023-09-30 14:36:51 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,166 @@
+#arthematic operator
+#addition
+a= 10
+b = 12
+print(a+b)
+
+
+# substraction
+a = 12
+b = 20
+print(a-b)
+# multipication
+
+a= 25
+b = 12
+print(a*b)
+
+# division
+a= 12
+b=13
+print(a%b)
+
+#floor division
+a=23
+b=14
+print(a//b)
+
+# assignment opertors
+#+=equal
+a=10
+a+= 1
+print(a)
+#-=
+a= 12
+a-=13
+print(a)
+#*=
+a=12
+a*=14
+print(a)
+#%=
+a=16
+a%=15
+print(a)
+# /=
+a=67
+a/=13
+print(a)
+#//=
+a= 13
+a//=12
+print(a)
+
+# logical operators
+# and
+a=10
+b=3
+c=9
+print(a>b and a>c)
+# or
+a= 13
+b=19
+c= 20
+print(a<b or a>c)
+
+#not operator
+a=10
+b=19
+c=13
+print(not(a<b and (a<c)))
+# comparsion operators
+#<=
+a=20
+b=10
+print(a<=b)
+#>=
+a=10
+b=19
+print(a>=b)
+#!=
+a=9
+b=8
+print(a!=b)
+
+#>
+a=10
+b=12
+print(a>b)
+#<
+a=12
+b=13
+print(a<b)
+
+#bitwise operator
+#and operator
+a=2
+b=10
+print(a&b)
+#or operator
+a= 10
+b= 2
+print(a or b)
+#2
+a=10
+b=2
+print(a|b)
+#xor operator
+a=10
+b=12
+print(a^b)
+
+#left shift operator
+#>>
+a=4
+b=2
+print(a>>b)
+a=5
+b=8
+print(a>>b)
+#right shift operator
+a=8
+b=9
+print(a<<b)
+a=1
+b=2
+print(a<<b)
+#check address of a variable
+a=111
+print(id(a))
+a=890
+print(id(a))
+#identity operator
+#is
+a=50
+b=40
+print(a is b)
+a=30
+b=30
+print(a is b)
+
+#is not
+a=10
+b=19
+print(a is not b)
+a=13
+b=13
+print(a is not b)
+
+#membership operator
+#in
+a="jyothi@gmail.com"
+print("@" in a)
+print("jyothi" in a)
+print("9" in a)
+fastfood=["biryani", "fired rise", "noodles"]
+name = "biryani"
+print(name in fastfood)
+name = "jyothi"
+print(name in fastfood)
+#not in
+names=["swarna" ,"amani" ,"sathya"]
+name = "jyothi"
+print(name not in names)
+name = "swarna"
+print(name not in names)
+
